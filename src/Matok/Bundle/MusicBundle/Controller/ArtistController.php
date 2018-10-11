d51af29ee25c9c4a82f09b7955b817b801faf5dc
Matej Kuna <mat.kuna@gmail.com>
2018-10-11 19:55:06 +0100

    set up acceptance tests

---
+++ src/Matok/Bundle/MusicBundle/Controller/ArtistController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Matok\Bundle\MusicBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArtistController extends Controller
+{
+    public function listAction(Request $request): Response
+    {
+    }
+
+    public function addAction(Request $request): Response
+    {
+    }
+
+    public function editAction(Request $request): Response
+    {
+    }
+
+    public function deleteAction(Request $request): Response
+    {
+    }
+}
