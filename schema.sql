4971401b0ffedc9b2a258b7d272464f9c5a6e20e
Matej Kuna <mat.kuna@gmail.com>
2018-10-14 18:48:20 +0200

    new schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Adminer 4.2.5 MySQL dump
+-- Adminer 4.3.1 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -10,20 +10,22 @@ CREATE TABLE `album` (
   `album_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `artist_id` int(11) unsigned DEFAULT NULL,
   `title` varchar(128) NOT NULL,
+  `genre` varchar(64) DEFAULT NULL,
   `year` smallint(5) unsigned DEFAULT NULL,
   PRIMARY KEY (`album_id`),
   KEY `artist_id` (`artist_id`),
-  CONSTRAINT `album_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `album` (`album_id`)
+  CONSTRAINT `album_ibfk_2` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `artist`;
 CREATE TABLE `artist` (
-  `artist_id` int(11) unsigned NOT NULL,
+  `artist_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(128) NOT NULL,
-  `genre` varchar(64) NOT NULL,
-  `web_page` varchar(128) NOT NULL
+  `web_page` varchar(512) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`artist_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2018-10-11 20:43:00
+-- 2018-10-14 16:47:37
